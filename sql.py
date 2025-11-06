757efe7184158502d0c597bcfa21b8f65791960e
Anirud <anirud25@gmail.com>
2025-11-06 17:41:49 +0530

    First commit with basic project structure

---
+++ sql.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+import google.generativeai as genai
+import os
+import sqlite3
+
---
+++ sqlite.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+conn=sqlite3.connect('student.db')
+cursor=conn.cursor()
+
+table_info="""
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY, 
+    NAME VARCHAR(25),
+    CLASS VARCHAR(26),
+    SECTION VARCHAR(25));
+"""
+
+cursor.execute(table_info)
+
+
+cursor.execute("INSERT INTO students (NAME, CLASS, SECTION) VALUES ('Alice', '10th Grade', 'A')")
+cursor.execute("INSERT INTO students (NAME, CLASS, SECTION) VALUES ('Bob', '10th Grade', 'B')")
+cursor.execute("INSERT INTO students (NAME, CLASS, SECTION) VALUES ('Charlie', '9th Grade', 'A')")
+
+print("Data inserted successfully")
+print("The printed records are: ")
+# data = cursor.execute("SELECT * FROM students").fetchall()
+# print(data)
+
+data = cursor.execute("SELECT * from students")
+for row in data:
+    print(row)
+               
